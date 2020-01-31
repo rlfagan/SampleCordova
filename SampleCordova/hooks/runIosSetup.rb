be483a88753c1942d36a7b7e991319633c717f2d
Omar Hernandez <ohernandez@us.ibm.com>
2020-01-31 14:46:00 -0800

    EAS-272: Added layout file to install script.

--- SampleCordova/hooks/runIosSetup.rb
+++ SampleCordova/hooks/runIosSetup.rb
@@ -7,6 +7,12 @@ podOrig = "#{currentDir}/platforms/ios/Podfile"
 podNew = "#{currentDir}/hooks/Podfile"
 puts "Copy #{podNew} to #{podOrig}"
 FileUtils.cp_r podNew, podOrig, remove_destination: true
+
 puts "cd #{currentDir}/platforms/ios && pod install"
 output = `cd #{currentDir}/platforms/ios && pod install`
 puts output
+
+layOrig = "#{currentDir}/platforms/ios/Pods/IBMTealeafDebug/SDKs/iOS/Debug/Tealeaf.framework/TLFResources.bundle/TealeafLayoutConfig.json"
+layNew = "#{currentDir}/hooks/TealeafLayoutConfig.json"
+puts "Copy #{layNew} to #{layOrig}"
+FileUtils.cp_r layNew, layOrig, remove_destination: true
